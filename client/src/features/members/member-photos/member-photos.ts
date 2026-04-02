6e5cae77d2b508b3cb91706d10a6d0ca2904f458
Bozhidar Ivanov <145542302+Bozho94@users.noreply.github.com>
2026-04-02 20:10:38 +0300

    End of section 12

--- client/src/features/members/member-photos/member-photos.ts
+++ client/src/features/members/member-photos/member-photos.ts
@@ -36,6 +36,9 @@ export class MemberPhotos implements OnInit {
         this.memberService.editMode.set(false);
         this.loading.set(false);
         this.photos.update(photos => [...photos, photo]);
+        if (!this.memberService.member()?.imageUrl) {
+          this.setMainLocalPhoto(photo);
+        }
       },
       error: error => {
         console.log('Error uploading photo:', error);
@@ -47,13 +50,7 @@ export class MemberPhotos implements OnInit {
   setMainPhoto(photo: Photo) {
     this.memberService.setMainPhoto(photo).subscribe({
       next: () => {
-        const currentUser = this.accountService.currentUser();
-        if (currentUser) currentUser.imageUrl = photo.url;
-        this.accountService.setCurrentUser(currentUser as User);
-        this.memberService.member.update(member => ({
-          ...member,
-          imageUrl: photo.url
-        }) as Member);
+        this.setMainLocalPhoto(photo);
       }
     })
   }
@@ -64,5 +61,15 @@ export class MemberPhotos implements OnInit {
         this.photos.update(photos => photos.filter(p => p.id !== photoId));
       }
     })
+  }
+
+  private setMainLocalPhoto(photo: Photo) {
+     const currentUser = this.accountService.currentUser();
+        if (currentUser) currentUser.imageUrl = photo.url;
+        this.accountService.setCurrentUser(currentUser as User);
+        this.memberService.member.update(member => ({
+          ...member,
+          imageUrl: photo.url
+        }) as Member);
   }
 }
